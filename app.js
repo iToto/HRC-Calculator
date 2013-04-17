dc74ca345be092e46a6076b66ed5737334fa9e1f
Salvatore D'Agositno <toto19@gmail.com>
2013-04-16 21:35:51 -0400

    added route for /calculate

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ express.compiler.compilers.less.compile = function(str, fn){
 // Routes
 
 app.get('/', routes.index);
+app.post('/calculate', routes.calculate);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
