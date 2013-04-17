58d57475fbb9e823258d05fa0771ad6321780ebf
Salvatore D'Agositno <toto19@gmail.com>
2013-04-16 22:38:39 -0400

    added heroku port

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ app.post('/calculate', function (req,res){
   routes.calculate(res,req.body.total);
 });
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
